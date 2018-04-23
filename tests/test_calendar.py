1b10d5e26fe410a9594ec76930520a8f197ce41f
dbarotzarena <36311694+dbarotzarena@users.noreply.github.com>
2018-04-23 18:44:07 -0400

    Update test_calendar.py

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -1,6 +1,6 @@
 import sys
 import os.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'app')))
 import unittest
 from main import app
 from app.models import *
